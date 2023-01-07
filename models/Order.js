18ac8144f511dcac3649a9e4e31f292af102db96
agbobli isaac <agbobliisaackwadzo@gmail.com>
2023-01-07 17:58:21 +0000

    Add database model

---
+++ models/Order.js
@@ -0,0 +1,13 @@
+/* import mongoose from 'mongoose';
+
+const orderSchema = new mongoose.Schema(
+      name:{
+        type:String,
+        required: true
+      },
+     
+
+);
+
+
+export default mongoose.Order || mongoose.model('Order',orderSchema); */
---
+++ models/Product.js
@@ -0,0 +1,16 @@
+/* import mongoose from 'mongoose' ;
+
+const productSchema = new mongoose.Schema(
+       name:{
+         type:String,
+         required:true
+       },
+       prices:{
+        type:[Number],
+       },
+       extraOptions:{
+          type:[number]
+       }
+)
+
+export default mogoose.Product || mongoose.models('Product', productSchema); */
